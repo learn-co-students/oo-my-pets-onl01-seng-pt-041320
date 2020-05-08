c9352aa7240979ed8a9add84b94d4003a8f5bd1c
Dice-K39 <daisukehashimoto39@gmail.com>
2020-05-08 19:33:17 -0400

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,21 @@
+require 'pry'
 class Cat
-  # code goes here
+  attr_accessor :owner, :mood
+  attr_reader :name
+  @@all = []
+
+  def initialize(pet_name, owner)
+    @name = pet_name
+    @owner = owner
+    @mood = "nervous"
+    @@all << self
+  end
+
+  def mood
+    @mood
+  end
+
+  def self.all
+    @@all
+  end
 end
