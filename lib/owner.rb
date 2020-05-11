545698f1749ecc80accd7828d3a91c59c9f4876f
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-05-10 21:42:25 -0700

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,48 @@
+require 'pry'
 class Owner
   # code goes here
+  attr_reader :name, :species
+
+  @@all = []
+  @@pets = {:dogs => [], :cats => []}
+
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all << self
+  end
+
+  def say_species
+    return "I am a #{@species}."
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@all.length
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+  def cats
+    Cat.all
+  end
+
+  def dogs
+    Dog.all
+  end
+
+  def buy_cat(cat)
+    #binding.pry
+    @@pets[:cats] << Cat.new(cat, self.name)
+  end
+
+  def buy_dog(dog)
+    @@pets[:dogs] << Dog.new(dog, self.name)
+  end
+
 end
