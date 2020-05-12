10db62322d1343b3c048cb41a54389e3f11dba29
Lon Bitner <lonbitner@gmail.com>
2020-05-12 03:51:20 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,23 @@
 class Dog
-  # code goes here
+  
+  @@all = []
+
+  attr_accessor :owner, :mood
+  attr_reader :name
+
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    @@all << self
+    
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def buy_dog(buy_dog)
+    dog.owner == self
+  end
 end
