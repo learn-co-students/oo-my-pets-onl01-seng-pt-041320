c529d563538d1f56586e43d88ba87e6bfa24805c
IfeAkiWad <ifeoluwa.wade@gmail.com>
2020-05-13 22:58:07 -0500

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,42 @@
+require 'pry'
+
 class Owner
-  # code goes here
+
+@@owners = []
+
+attr_accessor 
+attr_reader :name, :species
+
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@owners << self
+  end
+
+  def say_species
+    if @species == "human"
+       return "I am a human."
+    else
+       nil
+    end
+    # binding.pry
+  end
+
+  def self.all
+    @@owners
+  end
+
+  def self.count
+    self.all.count
+  end
+
+  def self.reset_all
+      self.all.clear
+    end
+
+    def cats
+      Cats.all
+    end
 end
+
+
