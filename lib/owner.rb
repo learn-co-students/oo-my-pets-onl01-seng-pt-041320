d9196e287ed98e69304203eafe623341cd40d72f
Cailie Skelton <cskelton707@gmail.com>
2020-05-25 02:28:08 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -33,7 +33,34 @@ class Owner
     Dog.all.select {|dog| dog.owner == self}
   end
   
-  def buy_cat(cat_name)
-    Cat.new(cat_name) << Cat.all.select
+  def buy_cat(name)
+    Cat.new(name, self)
+  end
+  
+  def buy_dog(name)
+    Dog.new(name, self)
+  end
+  
+  def walk_dogs
+   self.dogs.each {|dog| dog.mood = "happy"}
+  end
+  
+  def feed_cats
+    self.cats.each {|cat| cat.mood = "happy"}
+  end
+  
+  def sell_pets
+    self.cats.each do |cat|
+    cat.owner = nil
+    cat.mood = "nervous"
+    end
+    self.dogs.each do |dogs|
+    dogs.owner = nil
+    dogs.mood = "nervous"
+  end
+ end
+
+  def list_pets
+    "I have #{dogs.length} dog(s), and #{cats.length} cat(s)."
   end
 end
